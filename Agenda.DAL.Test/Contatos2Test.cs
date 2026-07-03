65ee27a6836e778fa313c8c480516ae0059a85b9
Mateus Paulino <mateusgpaulino@gmail.com>
2026-07-03 17:09:38 -0300

    adicionando sessoes para testes em bd

---
+++ Agenda.DAL.Test/Contatos2Test.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.DependencyModel;
+using NUnit.Framework;
+using Agenda.Domain;
+using AutoFixture;
+
+namespace Agenda.DAL.Test
+{
+    [TestFixture]
+    public class Contatos2Test : BaseTest
+    {
+        Contatos _contatos;
+        Fixture _fixture;
+
+        [SetUp]
+        public void Setup()
+        {
+            _contatos = new Contatos();
+            _fixture = new Fixture();
+        }
+
+        //ObterContatoTest
+        [Test]
+        public void ObterTodosOsContatoTest()
+        {
+            //arrange
+            var contato1 = _fixture.Create<Contato>();
+            var contato2 = _fixture.Create<Contato>();
+
+            //act
+            _contatos.Adicionar(contato1);
+            _contatos.Adicionar(contato2);
+            var listContato = _contatos.ObterTodos();
+            var contatoResultado = listContato.Where(x => x.Id == contato1.Id).First();
+
+            //assert
+            Assert.AreEqual(2, listContato.Count());
+            Assert.AreEqual(contato1.Id, contatoResultado.Id);
+            Assert.AreEqual(contato1.Nome, contatoResultado.Nome);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            _contatos = null;
+            _fixture = null;
+        }
+    }
+}
--- Agenda.DAL.Test/ContatosTest.cs
+++ Agenda.DAL.Test/ContatosTest.cs
@@ -8,13 +8,14 @@ namespace Agenda.DAL.Test
     [TestFixture]
     public class ContatosTest : BaseTest
     {
-        private Contatos _contatos;
-        private Fixture _fixture;
+        Contatos _contatos;
+        Fixture _fixture;
 
         [SetUp]
         public void Setup()
         {
             _contatos = new Contatos();
+            _fixture = new Fixture();
         }
 
         //AdicionarContatoTest
@@ -22,11 +23,7 @@ namespace Agenda.DAL.Test
         public void AdicionarContatoTest()
         {
             //arrange
-            var contato = new Contato()
-            {
-                Id = Guid.NewGuid(),
-                Nome = "Mateus"
-            };
+            var contato = _fixture.Create<Contato>();
 
             //act
             _contatos.Adicionar(contato);
@@ -40,12 +37,7 @@ namespace Agenda.DAL.Test
         public void ObterContatoTest()
         {
             //arrange
-            var contato = new Contato()
-            {
-                Id = Guid.NewGuid(),
-                Nome = "Mateus"
-            };
-
+            Contato contato = _fixture.Create<Contato>();
             Contato contatoResultado;
 
             //act
@@ -56,31 +48,12 @@ namespace Agenda.DAL.Test
             Assert.AreEqual(contato.Id, contatoResultado.Id);
             Assert.AreEqual(contato.Nome, contatoResultado.Nome);
         }
-        
-        //ObterContatoTest
-        [Test]
-        public void ObterTodosOsContatoTest()
-        {
-            //arrange
-            var contato1 = new Contato() {Id = Guid.NewGuid(),Nome = "Mateus"};
-            var contato2 = new Contato() {Id = Guid.NewGuid(),Nome = "Maria"};
-
-            //act
-            _contatos.Adicionar(contato1);
-            _contatos.Adicionar(contato2);
-            var listContato = _contatos.ObterTodos();
-            var contatoResultado = listContato.Where(x => x.Id == contato1.Id).First();
-
-            //assert
-            Assert.IsTrue(listContato.Count() > 1);
-            Assert.AreEqual(contato1.Id, contatoResultado.Id);
-            Assert.AreEqual(contato1.Nome, contatoResultado.Nome);
-        }
 
         [TearDown]
         public void Teardown()
         {
             _contatos = null;
+            _fixture = null;
         }
     }
 }
