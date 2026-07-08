f0962dab17496449fca9434bb42eebb4fe8d9eb7
Mateus Paulino <mateusgpaulino@gmail.com>
2026-07-08 20:17:47 -0300

    criando projeto de teste para os repositorios

---
+++ Agenda.Repos.Test/RepositorioContatosTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agenda.Repos.Test
+{
+    [TestFixture]
+    public class RepositorioContatosTest
+    {
+        [SetUp] 
+        public void SetUp() { }
+
+        [Test]
+        public void Test()
+        {
+
+        }
+        
+        [TearDown]
+        public void TearDown() { }
+    }
+}
