f32b620c1fee296f40bc7ea49bb496241f4a81e8
Mateus Paulino <mateusgpaulino@gmail.com>
2026-07-09 01:38:51 -0300

    adicionando o FluentValidation

---
+++ Agenda.Domain/ContatoValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agenda.Domain
+{
+    public class ContatoValidator : AbstractValidator<Contato>
+    {
+        public ContatoValidator()
+        {
+            RuleFor(x => x.Nome).NotEmpty().WithName("Nome do Contato").WithMessage("O {PropertyName} do contato é obrigatório.");
+            RuleFor(x => x.Nome).MinimumLength(6).WithMessage("O {PropertyName} do contato deve ter pelo menos 6 caracteres.");
+            RuleFor(x => x.Nome).MaximumLength(100).WithMessage("O {PropertyName} do contato deve ter no máximo 100 caracteres.");
+            RuleFor(x => x.Id).NotEmpty().WithName("ID do Contato").WithMessage("O {PropertyName} do contato é obrigatório.");
+            RuleFor(x => x.Id).Must(id => id != Guid.Empty).WithMessage("O {PropertyName} do contato não pode ser vazio.");
+        }
+    }
+}
